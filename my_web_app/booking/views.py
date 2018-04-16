1923bd0a1eee19c82d8e35bb4dda2d6b4ddc60f3
Your Name <you@example.com>
2018-04-17 01:13:01 +0300

    csrf

--- my_web_app/booking/views.py
+++ my_web_app/booking/views.py
@@ -4,6 +4,7 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Order, Room
 
@@ -15,6 +16,7 @@ def current_orders(request, year, month, day):
     context = {'orders': current_orders}
     return render(request, 'booking/current_orders.html', context)
 
+@csrf_exempt
 def create_room(request):
     r = json.loads(request.body.decode("utf-8"))
     room = Room(number=r['number'], price=r['price'])
@@ -25,11 +27,13 @@ def read_room(request, room_id):
     room = Room.objects.get(id=room_id)
     return JsonResponse(model_to_dict(room))
 
+@csrf_exempt
 def update_room(request, room_id):
     r = json.loads(request.body.decode("utf-8"))
     Room.objects.filter(id=room_id).update(**r)
     return JsonResponse(r)
 
+@csrf_exempt
 def delete_room(request, room_id):
     Room.objects.filter(id=room_id).delete()
     return HttpResponse()
